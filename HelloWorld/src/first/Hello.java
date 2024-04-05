a9cc9cfa7aa2bb3d8a6ae8fa07788d17fb71128b
tokosugimoto <toko.sugimoto.l021@dirbato.co.jp>
2024-04-05 14:59:53 +0900

    テスト

--- HelloWorld/src/first/Hello.java
+++ HelloWorld/src/first/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        
+        System.out.println("こんにちは、世界！")
+        System.out.println("aa")
 
     }
 
